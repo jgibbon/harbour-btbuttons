d88ea5d421755a468a2764052d382d3d07f0f102
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2019-12-03 01:48:32 +0800

    Create harbour-btbuttons-zh_CN.ts

---
+++ translations/harbour-btbuttons-zh_CN.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Mpris proxy is running.</source>
+        <translation>正在运行 Mpris 代理</translation>
+    </message>
+    <message>
+        <source>Mpris proxy isn&apos;t running.</source>
+        <translation>Mpris 代理未运行。</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>Mpris proxy is running.</source>
+        <translation>Mpris 代理正在运行。</translation>
+    </message>
+    <message>
+        <source>Mpris proxy isn&apos;t running.</source>
+        <translation>Mpris 运行未运行。</translation>
+    </message>
+    <message>
+        <source>BTtons</source>
+        <translation>Btons</translation>
+    </message>
+    <message>
+        <source>Close Proxy</source>
+        <translation>关闭代理</translation>
+    </message>
+    <message>
+        <source>Start Proxy</source>
+        <translation>开启代理</translation>
+    </message>
+</context>
+</TS>
